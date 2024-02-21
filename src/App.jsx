dca7a2bbf214eff81b36b23e47c5889c7bcc32b0
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-02-21 12:05:14 +0600

    import,export,alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
-import Gallery from "./Module-1.4/Gallery"
+import Gallery, { Profile as Image } from "./Module-1.5/Gallery"
+// import {Profile} from "./Module-1.5/Gallery"
 
 export default function App() {
   return (
     <Fragment>
       <Gallery />
+      <Image />
     </Fragment>
   )
 }
---
+++ src/Module-1.5/Gallery.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+export function Profile(){
+    return <img src="https://placehold.co/300" />
+}
+
+export default function Gallery(){
+    return(
+        <Fragment>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, architecto!</h2>
+        </Fragment>
+    )
+}
